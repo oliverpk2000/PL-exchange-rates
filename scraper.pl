f3486619f2edfe378476fc96284427c54b87423a
opk <oliver.pecek@gmail.com>
2024-07-07 17:01:18 +0200

    added dependency script, html data of page now displays in terminal

--- scraper.pl
+++ scraper.pl
@@ -1,4 +1,15 @@
 use strict;
 use warnings;
 
-print "hello world\n";
+use HTTP::Tiny;
+use HTML::TreeBuilder;
+
+exit main();
+
+sub main{
+    my $http = HTTP::Tiny->new();
+    my $response = $http->get('https://www.x-rates.com/table/?from=EUR&amount=1');
+    my $html_content = $response->{content};
+    
+    print "$html_content\n";
+}
